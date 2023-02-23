a96c345c080814021f189cf784212ed599d0311e
Francisco Chicano <chicano@lcc.uma.es>
2023-02-23 07:48:18 +0100

    modificación menor

--- src/main/java/es/uma/informatica/sii/spring/jpa/demo/Application.java
+++ src/main/java/es/uma/informatica/sii/spring/jpa/demo/Application.java
@@ -1,10 +1,7 @@
 package es.uma.informatica.sii.spring.jpa.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Application {
